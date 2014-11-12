6510773398394e94a3b87912fef55b49122c6538
beckmi <beckmi@c17157d5-08f2-41e7-a131-df055f6a0da3>
2014-11-12 12:20:01 +0000

    Adding eGuide module

---
+++ XoopsModules/eguide/trunk/eguide/language/english/blocks.php
@@ -0,0 +1,18 @@
+<?php
+// $Id$
+
+if (defined("_BLOCK_DATE_FMT")) return;
+
+// Blocks
+define("_BLOCK_DATE_FMT", "m/d");
+define("_BLOCK_EV_POST", "Posted");
+define("_BLOCK_EV_WAIT", "New Events");
+define("_BLOCK_EV_STYLE","Display Full");
+define("_BLOCK_EV_ITEMS","Display events in block");
+define("_BLOCK_EV_TRIM", "Limit length title strings");
+define("_BLOCK_EV_NONE", "There are currently NO events");
+define("_BLOCK_EV_LISTTYPE", "List type");
+define("_BLOCK_EV_TYPES", "All|Recent|Finish");
+define("_BLOCK_EV_CATEGORY", "Category");
+define("_BLOCK_EV_MORE", "more...");
+?>
